08345ac97297792de92bcd73b4530e23a1131ba7
${iawang123456} <${1115340631@qq.com}>
2022-05-07 06:29:46 +0800

    controller层处理网页发送过来的数据跟底层存储数据是否相同, 返回结果

--- springboot-demo/src/main/java/com/wang/controller/StudentController.java
+++ springboot-demo/src/main/java/com/wang/controller/StudentController.java
@@ -1,5 +1,16 @@
 package com.wang.controller;
 
 
+import com.wang.domain.Student;
+import com.wang.service.StudentService;
+
 public class StudentController {
+
+    private StudentService studentService;
+
+    public String selectOneStudent(Student student) {
+
+        Student studentOne = studentService.cha(student.getSid());
+        return studentOne.toString();
+    }
 }
