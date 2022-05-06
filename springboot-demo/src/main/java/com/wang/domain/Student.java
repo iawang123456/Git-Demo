667f00a12415318828bed90704fda87b57392ac6
${iawang123456} <${1115340631@qq.com}>
2022-05-07 06:40:54 +0800

    底层数据单个类

--- springboot-demo/src/main/java/com/wang/domain/Student.java
+++ springboot-demo/src/main/java/com/wang/domain/Student.java
@@ -1,5 +1,40 @@
 package com.wang.domain;
 
 public class Student {
+    private String sid;
+    private String sname;
+    private String ssex;
 
+    public Student() {
+    }
+
+    public Student(String sid, String sname, String ssex) {
+        this.sid = sid;
+        this.sname = sname;
+        this.ssex = ssex;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getSsex() {
+        return ssex;
+    }
+
+    public void setSsex(String ssex) {
+        this.ssex = ssex;
+    }
 }
