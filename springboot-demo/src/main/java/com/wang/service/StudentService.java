c6d7c8896cd2c01b2244bce995c1de08546bac55
${iawang123456} <${1115340631@qq.com}>
2022-05-07 06:34:10 +0800

    service层处理逻辑,底层过来的数据做处理, 处理完成之后给结果返回给控制层controller

--- springboot-demo/src/main/java/com/wang/service/StudentService.java
+++ springboot-demo/src/main/java/com/wang/service/StudentService.java
@@ -1,4 +1,13 @@
 package com.wang.service;
 
+import com.wang.dao.StudentDao;
+import com.wang.domain.Student;
+
 public class StudentService {
+
+    private StudentDao studentDao;
+
+    public Student cha(String sid) {
+        return studentDao.selectOne(sid);
+    }
 }
